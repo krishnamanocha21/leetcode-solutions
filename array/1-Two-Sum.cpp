d34f53c3d304132622d3bf995fe7fef1b65d2aa2
Krishna Manocha <kmanocha2106@gmail.com>
2026-07-03 17:33:35 +0530

    qspacing done

--- array/1-Two-Sum.cpp
+++ array/1-Two-Sum.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //hashmap technique 
-
-        
         for (int i=0;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
             int sum=nums[i]+nums[j];
             if (sum==target){
-                return {i,j};
-                
+                return {i,j};      
             }
-            }
-        }
-
-
-        
+          }
+        }        
         return {};
     }
 };
+
 
 /*
 not work because index number change on sorting
 
 while(j>i){
             int sum =nums[i]+nums[j];
-
             if(sum==target){
                 ans.push_back(i);
                 ans.push_back(j);
@@ -35,6 +28,5 @@ while(j>i){
                 i++;
             }
             else{j--;}
-
-        }
+            }
 */
