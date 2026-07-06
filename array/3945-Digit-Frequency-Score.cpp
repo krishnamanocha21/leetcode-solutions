71bc916adb8e07a71f6c6053021c082d5ad2b5f4
Krishna Manocha <kmanocha2106@gmail.com>
2026-07-07 00:31:46 +0530

    [3945] [Time Beats: 17.58%] [Memory Beats: 6.23%] - LeetPush

--- array/3945-Digit-Frequency-Score.cpp
+++ array/3945-Digit-Frequency-Score.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        unordered_map<int ,int> freq;
-
+            
+    unordered_map<int ,int> freq;
         string str =to_string(n);
         int ans=0;
-
         for(char c:str){
             int num=c-'0';
 
             freq[num]++;
         }
-
         for(auto &it :freq){
             ans+= it.first *it.second;
         }
         return ans;
     }
 };
+
